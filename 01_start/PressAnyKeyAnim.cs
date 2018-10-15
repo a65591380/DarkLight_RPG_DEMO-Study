60c4b182f36f3300c4850d693a07b15cab647a51
a65591380 <37826158+a65591380@users.noreply.github.com>
2018-10-15 14:37:08 +0800

    Add files via upload

---
+++ 01_start/PressAnyKeyAnim.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PressAnyKeyAnim : MonoBehaviour {
+
+    private PlayableDirector pressAnyKey;
+    private float timer = 0f;
+    private GameObject button;
+    private bool isAnyKeyDown = false;
+
+    public float startTime = 4f;
+   
+
+    private void Awake()
+    {
+        pressAnyKey = GetComponent<PlayableDirector>();
+        button = transform.parent.Find("StartButtonGroup").gameObject;
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+
+        //4秒后显示pressKey提示
+        if(timer>startTime)
+        {
+            pressAnyKey.enabled = true;
+            
+            if (isAnyKeyDown == false && Input.anyKey)  //按下后显示button
+            {
+                ShowButton();
+            }
+        }
+		
+	}
+
+    //显示button
+    void ShowButton()
+    {
+        button.SetActive(true);
+        this.gameObject.SetActive(false);
+        isAnyKeyDown = true;
+        
+    }
+}
